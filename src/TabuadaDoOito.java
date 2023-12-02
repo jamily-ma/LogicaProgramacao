5a40d095b7496e1e16e6a203d511112fe16332f8
jamily-ma <jamilymsousa@gmail.com>
2023-12-02 10:49:35 -0300

    Escreva um algoritmo que calcule e imprima a tabuada do 8 (1 a 10)

---
+++ src/TabuadaDoOito.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TabuadaDoOito {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("--- Tabuada do 8 ---");
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i * 8);
+        }
+
+    }
+}
